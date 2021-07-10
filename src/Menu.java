9991dcf26777e0aab349f552990db6687ad573af
MullaiArgent <mullairajan2000@gmail.com>
2021-07-10 19:22:28 +0530

    Pin Verification and Amt field with the Button Specifications were added.

--- src/Menu.java
+++ src/Menu.java
@@ -26,16 +26,19 @@ public class Menu extends JFrame implements ActionListener {
     JButton but_s_one = new JButton();
     JButton but_s_two = new JButton();
     JButton but_s_three = new JButton();
-    JButton but_s_four = new JButton();
+    //JButton but_s_four = new JButton();
 
     JLabel pin_label = new JLabel();
-    JLabel action_label = new JLabel();
+    JLabel amt_label = new JLabel();
     JLabel help_label = new JLabel();
 
     public Integer p = 0;
+    Integer BalanceInt = 0;
     public Integer decider = 0;
+
     HashMap<Integer,String> data = new HashMap<>();
     JLayeredPane pane = new JLayeredPane();
+
 
     Menu(){
         data.put(4321,"987654");
@@ -51,24 +54,24 @@ public class Menu extends JFrame implements ActionListener {
         pin_label.setBounds(85,5,255,245);
         pin_label.setVerticalAlignment(JLabel.CENTER);
         pin_label.setHorizontalAlignment(JLabel.CENTER);
-        pin_label.setBackground(Color.red);
         pin_label.setOpaque(true);
         this.add(pin_label);
+
 
         but_s_one.setBounds(5,10,50,100);
         but_s_two.setBounds(5,120,50,100);
         but_s_three.setBounds(370,10,50,100);
-        but_s_four.setBounds(370,120,50,100);
+        //but_s_four.setBounds(370,120,50,100);
 
         this.add(but_s_one);
         this.add(but_s_two);
         this.add(but_s_three);
-        this.add(but_s_four);
+        //this.add(but_s_four);
 
         but_s_one.setBackground(Color.darkGray);
         but_s_two.setBackground(Color.darkGray);
         but_s_three.setBackground(Color.darkGray);
-        but_s_four.setBackground(Color.darkGray);
+        //but_s_four.setBackground(Color.darkGray);
 
         this.add(but_one);
         but_one.setBounds(5,255,100,50);
@@ -120,6 +123,10 @@ public class Menu extends JFrame implements ActionListener {
         but_ash.addActionListener(this);
         but_help.addActionListener(this);
 
+        but_s_one.addActionListener(this);
+        but_s_two.addActionListener(this);
+        but_s_three.addActionListener(this);
+
         but_one.setBackground(Color.GRAY);
         but_one.setBackground(Color.GRAY);
         but_two.setBackground(Color.GRAY);
@@ -138,95 +145,111 @@ public class Menu extends JFrame implements ActionListener {
         but_ash.setBackground(Color.GRAY);
         but_help.setBackground(Color.lightGray);
 
+
+
         this.setLayout(null);
         this.setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e){
-        if(e.getSource()==but_one){
+        if(BalanceInt>100000000){
+            JOptionPane.showMessageDialog(null,"Enter Maximum RS 20,000 for the transaction","Maximum Limit Exceeded",JOptionPane.WARNING_MESSAGE);
+        }
+        else if(e.getSource()==but_one){
             if(decider==0) {
                 p = (p * 10) + 1;
                 x_printer();
             }else if(decider==1){
-
+                BalanceInt = (BalanceInt * 10) + 1;
+                amt_label.setText("Rs"+BalanceInt);
             }
         }else if(e.getSource()==but_two){
             if(decider==0) {
                 p = (p * 10) + 2;
                 x_printer();
             }else if(decider==1){
-
+                BalanceInt = (BalanceInt * 10) + 2;
+                amt_label.setText("Rs"+BalanceInt);
             }
         }else if(e.getSource()==but_three){
             if(decider==0) {
                 p = (p * 10) + 3;
                 x_printer();
             }else if(decider==1){
-
+                BalanceInt = (BalanceInt * 10) + 1;
+                amt_label.setText("Rs"+BalanceInt);
             }
         }else  if(e.getSource()==but_cancel){
             this.dispose();
+
         }else if(e.getSource()==but_four){
             if(decider==0) {
                 p = (p * 10) + 4;
                 x_printer();
             }else if(decider==1){
-
+                BalanceInt = (BalanceInt * 10) + 4;
+                amt_label.setText("Rs"+BalanceInt);
             }
         }else  if(e.getSource()==but_five){
             if(decider==0) {
                 p = (p * 10) + 5;
                 x_printer();
             }else if(decider==1){
-
+                BalanceInt = (BalanceInt * 10) + 5;
+                amt_label.setText("Rs"+BalanceInt);
             }
         }else if(e.getSource()==but_six){
             if(decider==0) {
                 p = (p * 10) + 6;
                 x_printer();
             }else if(decider==1){
-
+                BalanceInt = (BalanceInt * 10) + 6;
+                amt_label.setText("Rs"+BalanceInt);
             }
         }else  if(e.getSource()==but_clear){
-            p = p/10;
-            x_printer();
+            if (decider==0){
+                p = p/10;
+                x_printer();
+            }else if(decider==1){
+                BalanceInt = (BalanceInt / 10);
+                amt_label.setText("Rs"+BalanceInt);
+            }
+
         }else if(e.getSource()==but_seven){
             if(decider==0) {
                 p = (p * 10) + 7;
                 x_printer();
             }else if(decider==1){
-
+                BalanceInt = (BalanceInt * 10) + 7;
+                amt_label.setText("Rs"+BalanceInt);
             }
         } else if(e.getSource()==but_eight){
             if(decider==0) {
                 p = (p * 10) + 8;
                 x_printer();
             }else if(decider==1){
-
+                BalanceInt = (BalanceInt * 10) + 8;
+                amt_label.setText("Rs"+BalanceInt);
             }
         }else if(e.getSource()==but_nine){
             if(decider==0) {
                 p = (p * 10) + 9;
                 x_printer();
             }else if(decider==1){
-
+                BalanceInt = (BalanceInt * 10) + 9;
+                amt_label.setText("Rs"+BalanceInt);
             }
         }else if(e.getSource()==but_enter){
-                if(data.containsKey(p)){
-                    decider = 1;
-                    pane.add(action_label,Integer.valueOf(0));
-                    action_label.setBounds(85,5,255,245);
-                    action_label.setBackground(Color.blue);
-                    action_label.setOpaque(true);
-                    this.add(action_label);
-                }else{
-                    JOptionPane.showMessageDialog(null,"Invalid Entry","Invalid Entry",JOptionPane.ERROR_MESSAGE);
-                    p=0; pin_label.setText("Enter the Pin Number :");
-                }
+                action_page();
 
         }else if(e.getSource()==but_zero){
-            p  = (p*10);
-            pin_label.setText(""+p);
+            if(decider==0) {
+                p = (p * 10);
+                pin_label.setText("" + p);
+            }else if(decider==1){
+                BalanceInt = BalanceInt * 10;
+                amt_label.setText("Rs"+BalanceInt);
+            }
         }else if(e.getSource()==but_help){
             pane.add(help_label,Integer.valueOf(2));
             help_label.setBounds(85,5,255,245);
@@ -241,6 +264,14 @@ public class Menu extends JFrame implements ActionListener {
                     " Press Any Number Key to continue</html>";
             help_label.setText(help);
 
+        }else if(e.getSource()==but_s_one){
+
+        }else if(e.getSource()==but_s_two){
+            decider=1;
+
+        }else if(e.getSource()==but_s_three){
+            decider=1;
+
         }
     }
     public void x_printer(){
@@ -253,6 +284,43 @@ public class Menu extends JFrame implements ActionListener {
             temp/=10;
             x += "X";
             pin_label.setText(x);
-        }
+    }}}
 
-}}}
+    public void action_page(){
+        if(data.containsKey(p)){
+            decider=2;
+            JPanel action_panel = new JPanel();
+            pane.add(action_panel,Integer.valueOf(0));
+            action_panel.setBounds(85,10,255,245);
+            action_panel.setBackground(Color.blue);
+            action_panel.setOpaque(true);
+            action_panel.setLayout(null);
+            this.add(action_panel);
+
+            JLabel view_bal = new JLabel();
+            view_bal.setText("View Balance");
+            view_bal.setBackground(Color.black);
+            view_bal.setBounds(30,10,100,100);
+            action_panel.add(view_bal);
+
+            JLabel amt_deposit = new JLabel();
+            amt_deposit.setText("Money Deposit");
+            amt_deposit.setBackground(Color.BLACK);
+            amt_deposit.setBounds(30,110,100,100);
+            action_panel.add(amt_deposit);
+
+            JLabel amt_withdraw = new JLabel();
+            amt_withdraw.setText("Money Withdraw");
+            amt_withdraw.setBackground(Color.BLACK);
+            amt_withdraw.setBounds(140,10,100,100);
+            action_panel.add(amt_withdraw);
+
+            amt_label.setBounds(140,140,200,40);
+            action_panel.add(amt_label);
+
+        }else{
+            JOptionPane.showMessageDialog(null,"Invalid Entry","Invalid Entry",JOptionPane.ERROR_MESSAGE);
+            p=0; pin_label.setText("Enter the Pin Number :");
+        }
+}
+    }
